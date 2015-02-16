960df75450da1d576f2219b4e13075ea3083d4b5
XiChaoSir <95877589@qq.com>
2015-02-16 20:19:45 +0800

    before git pull.

--- app/assets/javascripts/chao_users.js
+++ app/assets/javascripts/chao_users.js
@@ -1,2 +1,46 @@
 app.chao_user = { 
+  init : function() { 
+    this.initStatus();
+	this.bindingEvents();
+  },
+  
+  initStatus() : function() {
+  },
+  
+  bindingEvents() : function() {
+    $("body").on("click", ".s-new-submit", this.validateUserName);
+    $("body").on("change", "#chao_user_nick_name", this.checkNickName);
+  },
+
+  validateUserName : function(){
+	if($("#new-message").text() != ""){ return false;
+	}
+    
+    if($(".chao_user_nick_name").val() == ""){
+	  $("#new-message").text("Nick Name can't be nir.");
+	  return false;
+	}	
+  },
+
+  checkNickName : function() {
+	$.ajax({
+	  url : "/chao_users/check_name",
+	  date : {
+		"nick_name" : $("#chao_user_nick_name").val(),
+		"authenticity_token" : $("meta[name='csrf-token']").attr("content")
+	  },
+	  type : "GET",
+	  success : function(result) {
+	    if(result.is_exist){
+		  $("#new-message").text("Nick Name had been used."); 
+		}
+		else{
+		  $("#new-message").text("");
+		}
+	  }
+	  error : function() {
+         alert("Sorry,It is Error.")
+	  }
+	})
+  }
 }
